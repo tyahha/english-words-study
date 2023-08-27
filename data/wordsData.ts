cdacd3781a7ff5155eac984a13ff1fe5016da49e
toshinori sato <tyahhahha@gmail.com>
2023-08-27 12:12:40 +0900

    問題追加

--- data/wordsData.ts
+++ data/wordsData.ts
@@ -1,4 +1,5 @@
 import { MaruttoWordsData } from "@/data/001_M_marutto"
+import { FirstWordsData } from "@/data/002_T-1_first"
 
 export type WordDataType = {
   id: string
@@ -8,4 +9,7 @@ export type WordDataType = {
 
 export type QuestionType = "J2E" | "E2J" | "listening"
 
-export const WordsData: WordDataType[] = [...MaruttoWordsData]
+export const WordsData: WordDataType[] = [
+  ...MaruttoWordsData,
+  ...FirstWordsData,
+]
